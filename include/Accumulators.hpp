138cdb64e8012b256e324c5a28f4dad645e72711
R3tr0 <nikk3695@gmail.com>
2025-07-31 14:32:28 +0300

    Added licensing

--- include/Accumulators.hpp
+++ include/Accumulators.hpp
@@ -1,3 +1,13 @@
+/*
+    Copyright (C) 2023 Nikita Retr0-code Korneev
+
+    libgraph is free software: you can redistribute, modify it
+    under the terms of the MIT License.
+
+    You should have received a copy of MIT License along with jsast.js.
+    If not, see <https://opensource.org/license/MIT.
+*/
+
 #pragma once
 #include "GraphBase.hpp"
 
